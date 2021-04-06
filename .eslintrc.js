af92e4cf8246dbdfe81b7d9071456004dbc55457
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-04-06 12:24:05 +0300

    Added styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "node": true
-    },
-    "extends": [
-        "plugin:vue/vue3-essential",
-        "eslint:recommended"
-    ],
-    "parserOptions": {
-        "parser": "babel-eslint"
-    },
-    "rules": {
-        "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn"
-    }
-}
+  extends: [
+    "plugin:vue/vue3-essential",
+    "@vue/prettier",
+    "eslint:recommended",
+  ],
+
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+
+  rules: {
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="container">
+    <page-home />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-const name = "Mohjak"
+import PageHome from '@/components/PageHome'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    PageHome,
+  },
 }
 </script>
 
