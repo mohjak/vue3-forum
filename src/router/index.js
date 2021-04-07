1b9a6bd6e98a204da60c8fe3e9f08f14c325dbc4
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-04-07 11:35:40 +0300

    Showed not found page without changing the url

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import PageHome from '@/components/PageHome'
 import { createRouter, createWebHistory } from 'vue-router'
 import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
+import sourceData from '@/data.json'
 
 const routes = [
   {
@@ -13,7 +15,25 @@ const routes = [
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true,
+    beforeEnter(to, from, next) {
+      // check if thread exists
+      const trheadExists = sourceData.threads.find((thread) => thread.id === to.params.id)
+      // if exists continue
+      if (trheadExists) {
+        return next()
+      } else {
+        // if doesnt exists redirect to not found
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash,
+        })
+      }
+    },
   },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound },
 ]
 
 export default createRouter({
