9ef5b231eb23932bcd1ea86f56a0f7ff95762b8d
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-04-09 11:21:18 +0300

    Added actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -3,4 +3,20 @@ import sourceData from '@/data'
 
 export default createStore({
   state: sourceData,
+  actions: {
+    createPost(context, post) {
+      post.id = 'qqqq' + Math.random()
+      context.commit('setPost', { post }) // set the post
+      context.commit('appendPostToThread', { postId: post.id, threadId: post.threadId }) // append post to thread
+    }
+  },
+  mutations: {
+    setPost(state, { post }) {
+      state.posts.push(post)
+    },
+    appendPostToThread(state, { postId, threadId }) {
+      const threads = state.threads.find((thread) => thread.id === threadId)
+      threads.posts.push(postId)
+    },
+  }
 })
