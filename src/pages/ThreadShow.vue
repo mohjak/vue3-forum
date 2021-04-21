c38bde2462b3caf8ea524310ff56098dceef3b2b
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-04-21 15:12:03 +0300

    Creating dynamic vuex getters with higher order functions

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -4,6 +4,13 @@
       {{ thread.title }}
       <router-link :to="{ name: 'ThreadEdit', id: this.id }" class="btn-green btn-small" tag="button"> Edit Thread </router-link>
     </h1>
+    <p>
+      By <a href="#" class="link-unstyled">{{ thread.author.name }}</a
+      >, <AppDate :timestamp="thread.publishedAt" />.
+      <span style="float: right; margin-top: 2px" class="hide-mobile text-faded text-small"
+        >{{ thread.repliesCount }} replies by {{ thread.contributorsCount }} contributors</span
+      >
+    </p>
 
     <post-list :posts="threadPosts" />
 
@@ -14,7 +21,6 @@
 <script>
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
-import { findById } from '@/helpers'
 export default {
   name: 'ThreadShow',
   components: {
@@ -35,7 +41,7 @@ export default {
       return this.$store.state.threads
     },
     thread() {
-      return findById(this.threads, this.id) // also available under this.$route.params.id
+      return this.$store.getters.thread(this.id)
     },
     threadPosts() {
       return this.posts.filter((post) => post.threadId === this.id)
