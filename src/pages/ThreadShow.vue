03f97e472083c49e7175c892fbe9b5d9f569b388
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-04-26 18:44:29 +0300

    Migrating vuex to use cloud firestore database

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -5,7 +5,7 @@
       <router-link :to="{ name: 'ThreadEdit', id: this.id }" class="btn-green btn-small" tag="button"> Edit Thread </router-link>
     </h1>
     <p>
-      By <a href="#" class="link-unstyled">{{ thread.author.name }}</a
+      By <a href="#" class="link-unstyled">{{ thread.author?.name }}</a
       >, <AppDate :timestamp="thread.publishedAt" />.
       <span style="float: right; margin-top: 2px" class="hide-mobile text-faded text-small"
         >{{ thread.repliesCount }} replies by {{ thread.contributorsCount }} contributors</span
@@ -21,6 +21,7 @@
 <script>
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
+import firebase from 'firebase'
 export default {
   name: 'ThreadShow',
   components: {
@@ -55,6 +56,49 @@ export default {
       }
       this.$store.dispatch('createPost', post)
     },
+  },
+  created() {
+    // fetch the thread
+    firebase
+      .firestore()
+      .collection('threads')
+      .doc(this.id)
+      .onSnapshot((doc) => {
+        const thread = { ...doc.data(), id: doc.id }
+        this.$store.commit('setThread', { thread })
+
+        // fetch the user
+        firebase
+          .firestore()
+          .collection('users')
+          .doc(thread.userId)
+          .onSnapshot((doc) => {
+            const user = { ...doc.data(), id: doc.id }
+            this.$store.commit('setUser', { user })
+          })
+
+        // fetch the posts
+        thread.posts.forEach((postId) => {
+          firebase
+            .firestore()
+            .collection('posts')
+            .doc(postId)
+            .onSnapshot((doc) => {
+              const post = { ...doc.data(), id: doc.id }
+              this.$store.commit('setPost', { post })
+
+              // fetch the user for each post
+              firebase
+                .firestore()
+                .collection('users')
+                .doc(post.userId)
+                .onSnapshot((doc) => {
+                  const user = { ...doc.data(), id: doc.id }
+                  this.$store.commit('setUser', { user })
+                })
+            })
+        })
+      })
   },
 }
 </script>
