8d1097cb9ff74c254ae8c7cb97a2abe3dea9107d
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-04-26 15:07:33 +0300

    Init firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyBDsH9oHn68OfKqfPM-L-WsGcpqN31FJ2I',
+  authDomain: 'vue-school-forum-8249c.firebaseapp.com',
+  projectId: 'vue-school-forum-8249c',
+  storageBucket: 'vue-school-forum-8249c.appspot.com',
+  messagingSenderId: '573851958845',
+  appId: '1:573851958845:web:a23e0cc76944a2aaf2f454',
+}
