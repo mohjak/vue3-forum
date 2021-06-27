8136f1b00eda91adc62f5e0b1d21930cd4cef40b
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-06-27 16:10:53 +0300

    Prepared the User Interface for Post Updates.

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -1,29 +1,31 @@
 <template>
   <form @submit.prevent="save">
     <div class="form-group">
-      <textarea v-model="text" name="" id="" cols="30" rows="10" class="form-input" />
+      <textarea v-model="postCopy.text" name="" id="" cols="30" rows="10" class="form-input" />
     </div>
     <div class="form-actions">
-      <button class="btn-blue">Submit post</button>
+      <button class="btn-blue">{{ post.id ? 'Update Post' : 'Submit Post' }}</button>
     </div>
   </form>
 </template>
 
 <script>
 export default {
+  props: {
+    post: {
+      type: Object,
+      default: () => ({ text: null }),
+    },
+  },
   data() {
     return {
-      text: '',
+      postCopy: { ...this.post },
     }
   },
   methods: {
     save() {
-      const post = {
-        text: this.text,
-      }
-      this.$emit('save', { post }) // access under eventData.post
-
-      this.text = ''
+      this.$emit('save', { post: this.postCopy }) // access under eventData.post
+      this.postCopy.text = ''
     },
   },
 }
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -13,12 +13,19 @@
       </div>
 
       <div class="post-content">
-        <div>
-          <p>
+        <div class="col-full">
+          <PostEditor v-if="editing == post.id" :post="post" />
+          <p v-else>
             {{ post.text }}
           </p>
         </div>
-        <a href="#" style="margin-left: auto; padding-left: 10px" class="link-unstyled" title="Make a change">
+        <a
+          @click.prevent="toggleEditMode(post.id)"
+          href="#"
+          style="margin-left: auto; padding-left: 10px"
+          class="link-unstyled"
+          title="Make a change"
+        >
           <fa icon="pencil-alt" />
         </a>
       </div>
@@ -30,12 +37,21 @@
 </template>
 
 <script>
+import PostEditor from '@/components/PostEditor'
 export default {
+  components: {
+    PostEditor,
+  },
   props: {
     posts: {
       type: Array,
       required: true,
     },
+  },
+  data() {
+    return {
+      editing: null,
+    }
   },
   computed: {
     users() {
@@ -45,6 +61,9 @@ export default {
   methods: {
     userById(userId) {
       return this.$store.getters.user(userId)
+    },
+    toggleEditMode(id) {
+      this.editing = id === this.editing ? null : id
     },
   },
 }
